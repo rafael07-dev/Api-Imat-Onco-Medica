059d1f49e261f4c6e5c6974315bcebf3b7012870
rafael07-dev <deralato817@gmail.com>
2025-06-15 16:54:00 -0500

    add interface equipment service

--- src/main/java/com/imat/oncomedica/inventory_management/domain/service/EquipmentService.java
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/service/EquipmentService.java
@@ -1,4 +1,16 @@
 package com.imat.oncomedica.inventory_management.domain.service;
 
+import com.imat.oncomedica.inventory_management.application.dto.CreateEquipmentRequest;
+import com.imat.oncomedica.inventory_management.application.dto.EquipmentResponse;
+import com.imat.oncomedica.inventory_management.application.dto.UpdateEquipmentRequest;
+
+import java.util.List;
+
 public interface EquipmentService {
+
+    EquipmentResponse getEquipmentById(Integer id);
+    List<EquipmentResponse> getAllEquipments();
+    EquipmentResponse saveEquipment(CreateEquipmentRequest request);
+    EquipmentResponse updateEquipment(UpdateEquipmentRequest request, Integer id);
+    EquipmentResponse deleteEquipment(Integer id);
 }
