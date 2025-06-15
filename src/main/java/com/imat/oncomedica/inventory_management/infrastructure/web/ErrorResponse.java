390be93516d071da4cd2bf4a247bce13bc8b0cbe
rafael07-dev <deralato817@gmail.com>
2025-06-15 16:52:47 -0500

    add error response

--- src/main/java/com/imat/oncomedica/inventory_management/infrastructure/web/ErrorResponse.java
+++ src/main/java/com/imat/oncomedica/inventory_management/infrastructure/web/ErrorResponse.java
@@ -1,2 +1,20 @@
-package com.imat.oncomedica.inventory_management.infrastructure.web;public class ErrorResponse {
+package com.imat.oncomedica.inventory_management.infrastructure.web;
+
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class ErrorResponse {
+
+    private int status;
+    private String error;
+    private String message;
+    private LocalDateTime timestamp;
+
+    public ErrorResponse(int status, String error, String message, LocalDateTime timestamp) {
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.timestamp = timestamp;
+    }
 }
