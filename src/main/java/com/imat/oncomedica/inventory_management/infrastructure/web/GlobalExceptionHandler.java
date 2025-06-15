e6ed7e8fc93bde6177d68fb848f376f4009c437d
rafael07-dev <deralato817@gmail.com>
2025-06-15 16:52:29 -0500

    add exception handler

--- src/main/java/com/imat/oncomedica/inventory_management/infrastructure/web/GlobalExceptionHandler.java
+++ src/main/java/com/imat/oncomedica/inventory_management/infrastructure/web/GlobalExceptionHandler.java
@@ -1,2 +1,26 @@
-package com.imat.oncomedica.inventory_management.infrastructure.web;public class GlobalExceptionHandler {
+package com.imat.oncomedica.inventory_management.infrastructure.web;
+
+import com.imat.oncomedica.inventory_management.domain.exception.EquipmentNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EquipmentNotFoundException.class)
+    public ResponseEntity<ErrorResponse> equipmentNotFoundException(EquipmentNotFoundException e) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.NOT_FOUND.value(),
+                "Equipment Not Found",
+                e.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
 }
