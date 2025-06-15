108ed2ca84bc1cf6dc173be968c55a005833ea77
rafael07-dev <deralato817@gmail.com>
2025-06-15 16:53:38 -0500

    functional equipment controller

--- src/main/java/com/imat/oncomedica/inventory_management/infrastructure/web/EquipmentController.java
+++ src/main/java/com/imat/oncomedica/inventory_management/infrastructure/web/EquipmentController.java
@@ -1,8 +1,9 @@
 package com.imat.oncomedica.inventory_management.infrastructure.web;
 
-import com.imat.oncomedica.inventory_management.domain.entity.Equipment;
-import com.imat.oncomedica.inventory_management.application.dto.EquipmentDTO;
-import com.imat.oncomedica.inventory_management.application.service.EquipmentServiceImpl;
+import com.imat.oncomedica.inventory_management.application.dto.CreateEquipmentRequest;
+import com.imat.oncomedica.inventory_management.application.dto.EquipmentResponse;
+import com.imat.oncomedica.inventory_management.application.dto.UpdateEquipmentRequest;
+import com.imat.oncomedica.inventory_management.domain.service.EquipmentService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.net.URI;
@@ -12,36 +13,38 @@ import java.util.List;
 @RequestMapping("/api/equipments")
 public class EquipmentController {
 
-    private final EquipmentServiceImpl equipmentServiceImpl;
+    private final EquipmentService equipmentService;
 
-    public EquipmentController(EquipmentServiceImpl equipmentServiceImpl) {
-        this.equipmentServiceImpl = equipmentServiceImpl;
+    public EquipmentController(EquipmentService equipmentService) {
+        this.equipmentService = equipmentService;
     }
 
+
     @GetMapping("/")
-    public ResponseEntity<List<EquipmentDTO>> getAll(){
-        return ResponseEntity.ok().body(equipmentServiceImpl.getAllEquipments());
+    public ResponseEntity<List<EquipmentResponse>> getAll(){
+        return ResponseEntity.ok().body(equipmentService.getAllEquipments());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<EquipmentDTO> getEquipmentById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(equipmentServiceImpl.findById(id));
+    public ResponseEntity<EquipmentResponse> getEquipmentById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(equipmentService.getEquipmentById(id));
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<Equipment> createEquipment(@RequestBody Equipment equipment){
-        return ResponseEntity.created(URI.create("/api/equipments/" + equipment.getId()))
-                .body(equipmentServiceImpl.saveEquipment(equipment));
+    @PostMapping(value = "/create")
+    public ResponseEntity<EquipmentResponse> createEquipment(@RequestBody CreateEquipmentRequest equipment){
+        EquipmentResponse equipmentSaved = equipmentService.saveEquipment(equipment);
+        return ResponseEntity.created(URI.create("/api/equipments/" + equipmentSaved.getId()))
+                .body(equipmentSaved);
     }
 
     @PutMapping("/update/{idEquipment}")
-    public ResponseEntity<EquipmentDTO> updateEquipment(@PathVariable("idEquipment") Integer id,
-                                                        @RequestBody EquipmentDTO equipmentDTO){
-        return ResponseEntity.ok().body(equipmentServiceImpl.updateEquipment(id, equipmentDTO));
+    public ResponseEntity<EquipmentResponse> updateEquipment(@PathVariable("idEquipment") Integer id,
+                                                        @RequestBody UpdateEquipmentRequest equipment){
+        return ResponseEntity.ok().body(equipmentService.updateEquipment(equipment, id));
     }
 
     @DeleteMapping("/delete/{idEquipment}")
-    public ResponseEntity<EquipmentDTO> deleteEquipment(@PathVariable("idEquipment") Integer id){
-        return ResponseEntity.accepted().body(equipmentServiceImpl.deleteEquipment(id));
+    public ResponseEntity<EquipmentResponse> deleteEquipment(@PathVariable("idEquipment") Integer id){
+        return ResponseEntity.accepted().body(equipmentService.deleteEquipment(id));
     }
 }
