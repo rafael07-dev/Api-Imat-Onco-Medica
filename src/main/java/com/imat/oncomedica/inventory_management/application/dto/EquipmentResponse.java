9f26e4c2d6463fb21e404d1152a46daaeff8acaf
rafael07-dev <deralato817@gmail.com>
2025-06-17 11:36:45 -0500

    add image url field

--- src/main/java/com/imat/oncomedica/inventory_management/application/dto/EquipmentResponse.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/dto/EquipmentResponse.java
@@ -2,13 +2,16 @@ package com.imat.oncomedica.inventory_management.application.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EquipmentResponse {
 
     private Integer id;
     private String equipmentName;
+    private String imageUrl;
     private String type;
     private String inventoryCode;
     private String brand;
