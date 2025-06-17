9578961b05d3d4b8443ed4b8ea9058c5e2d34c1a
rafael07-dev <deralato817@gmail.com>
2025-06-17 11:39:25 -0500

    add use case to upload image to equipment

---
+++ src/main/java/com/imat/oncomedica/inventory_management/application/usecase/UploadEquipmentImageUseCase.java
@@ -0,0 +1,33 @@
+package com.imat.oncomedica.inventory_management.application.usecase;
+
+import com.imat.oncomedica.inventory_management.domain.entity.Equipment;
+import com.imat.oncomedica.inventory_management.domain.exception.EquipmentNotFoundException;
+import com.imat.oncomedica.inventory_management.domain.service.FileStorageService;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.EquipmentRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class UploadEquipmentImageUseCase {
+
+    private final EquipmentRepository repository;
+    private final FileStorageService fileStorageService;
+
+    public UploadEquipmentImageUseCase(EquipmentRepository repository, FileStorageService fileStorageService) {
+        this.repository = repository;
+        this.fileStorageService = fileStorageService;
+    }
+
+    public String execute(Integer equipmentId, MultipartFile file) {
+        Equipment equipment = repository.findById(equipmentId)
+                .orElseThrow(() -> new EquipmentNotFoundException(equipmentId));
+
+        String imageUrl = fileStorageService.upload(equipmentId, file);
+
+        equipment.setImageUrl(imageUrl);
+
+        repository.save(equipment);
+
+        return imageUrl;
+    }
+}
