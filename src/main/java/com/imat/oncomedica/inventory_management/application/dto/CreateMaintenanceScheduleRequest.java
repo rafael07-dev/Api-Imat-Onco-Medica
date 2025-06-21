0ae2fff139223b2df683b2c8567fb591022f49de
rafael07-dev <deralato817@gmail.com>
2025-06-21 15:40:59 -0500

    add dto to create maintenance schedule

--- src/main/java/com/imat/oncomedica/inventory_management/application/dto/CreateMaintenanceScheduleRequest.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/dto/CreateMaintenanceScheduleRequest.java
@@ -1,4 +1,11 @@
 package com.imat.oncomedica.inventory_management.application.dto;
 
+import lombok.Data;
+import java.util.List;
+
+@Data
 public class CreateMaintenanceScheduleRequest {
+    private Integer equipmentId;
+    private Integer maintenanceStaffId;
+    private List<CreateMonthlyMaintenanceRequest> months;
 }
