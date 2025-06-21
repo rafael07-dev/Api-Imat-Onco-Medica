db31e1c45276bd382bb802fb517d1b0f12b714a6
rafael07-dev <deralato817@gmail.com>
2025-06-21 15:43:55 -0500

    Update Maintenance Response to display only necessary information

--- src/main/java/com/imat/oncomedica/inventory_management/application/dto/MaintenanceScheduleResponse.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/dto/MaintenanceScheduleResponse.java
@@ -1,37 +1,20 @@
 package com.imat.oncomedica.inventory_management.application.dto;
 
-import com.imat.oncomedica.inventory_management.domain.entity.MaintenanceSchedule;
+import com.imat.oncomedica.inventory_management.domain.entity.Equipment;
+import com.imat.oncomedica.inventory_management.domain.entity.MaintenanceStaff;
 import com.imat.oncomedica.inventory_management.domain.entity.MonthlyMaintenance;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MaintenanceScheduleResponse {
     private Integer id;
-    private Integer equipmentId;
-    private String equipmentName;
-    private String inventoryCode;
-    private String brand;
-    private String model;
-    private String location;
-    private String type;
-    private String floor;
-    private String tower;
-    private String area;
+    private Equipment equipment;
+    private MaintenanceStaff responsible;
     private List<MonthlyMaintenance> monthlyMaintenances;
-
-    public MaintenanceScheduleResponse(MaintenanceSchedule maintenanceSchedule) {
-        this.id = maintenanceSchedule.getId();
-        this.equipmentId = maintenanceSchedule.getEquipment().getId();
-        /*this.equipmentName = maintenanceSchedule.getEquipmentName();
-        this.inventoryCode = maintenanceSchedule.getInventoryCode();
-        this.brand = maintenanceSchedule.getBrand();
-        this.model = maintenanceSchedule.getModel();
-        this.location = maintenanceSchedule.getLocation();
-        this.type = maintenanceSchedule.getType();
-        this.floor = maintenanceSchedule.getFloor();
-        this.tower = maintenanceSchedule.getTower();
-        this.area = maintenanceSchedule.getArea();*/
-        this.monthlyMaintenances = maintenanceSchedule.getMonthlyMaintenances();
-    }
 }
