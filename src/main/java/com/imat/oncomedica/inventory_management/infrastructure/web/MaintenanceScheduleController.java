1bf1d59a3f57eaa582c06ff8aba4db0132a8826e
rafael07-dev <deralato817@gmail.com>
2025-06-21 15:49:17 -0500

    injecting MaintenanceScheduleService into the controller

--- src/main/java/com/imat/oncomedica/inventory_management/infrastructure/web/MaintenanceScheduleController.java
+++ src/main/java/com/imat/oncomedica/inventory_management/infrastructure/web/MaintenanceScheduleController.java
@@ -1,46 +1,49 @@
 package com.imat.oncomedica.inventory_management.infrastructure.web;
 
-import com.imat.oncomedica.inventory_management.application.dto.MaintenanceScheduleDTO;
+import com.imat.oncomedica.inventory_management.application.dto.CreateMaintenanceScheduleRequest;
+import com.imat.oncomedica.inventory_management.application.dto.MaintenanceScheduleResponse;
+import com.imat.oncomedica.inventory_management.application.dto.UpdateMaintenanceScheduleRequest;
 import com.imat.oncomedica.inventory_management.application.service.MaintenanceScheduleServiceImpl;
+import com.imat.oncomedica.inventory_management.domain.service.MaintenanceScheduleService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/schedule")
 public class MaintenanceScheduleController {
 
-    private final MaintenanceScheduleServiceImpl maintenanceScheduleServiceImpl;
+    private final MaintenanceScheduleService maintenanceScheduleService;
 
     public MaintenanceScheduleController(MaintenanceScheduleServiceImpl maintenanceScheduleServiceImpl) {
-        this.maintenanceScheduleServiceImpl = maintenanceScheduleServiceImpl;
+        this.maintenanceScheduleService = maintenanceScheduleServiceImpl;
     }
 
     @GetMapping("/")
-    public ResponseEntity<List<Map<String, Object>>> getAllMaintenanceSchedules() {
-        return ResponseEntity.ok().body(maintenanceScheduleServiceImpl.findAllSchedules());
+    public ResponseEntity<List<MaintenanceScheduleResponse>> getAllMaintenanceSchedules() {
+        return ResponseEntity.ok().body(maintenanceScheduleService.getAllMaintenanceSchedules());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<List<Map<String, Object>>> getAllMaintenanceSchedules(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(maintenanceScheduleServiceImpl.findOneSchedule(id));
+    public ResponseEntity<MaintenanceScheduleResponse> getMaintenanceScheduleById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(maintenanceScheduleService.getMaintenanceScheduleById(id));
     }
 
     @PostMapping("/create")
-    public ResponseEntity<MaintenanceScheduleDTO> save(@RequestBody MaintenanceScheduleDTO maintenanceScheduleDTO) {
-        return ResponseEntity.created(URI.create("/api/schedule/" + maintenanceScheduleDTO.getId()))
-                .body(maintenanceScheduleServiceImpl.addSchedule(maintenanceScheduleDTO));
+    public ResponseEntity<MaintenanceScheduleResponse> save(@RequestBody CreateMaintenanceScheduleRequest request) {
+        var maintenanceCreated = maintenanceScheduleService.createMaintenanceSchedule(request);
+        return ResponseEntity.created(URI.create("/api/schedule/" + maintenanceCreated.getId()))
+                .body(maintenanceCreated);
     }
 
     @PatchMapping("/update/{id}")
-    public ResponseEntity<MaintenanceScheduleDTO> update(@RequestBody MaintenanceScheduleDTO dto, @PathVariable Integer id) {
-        return ResponseEntity.ok(maintenanceScheduleServiceImpl.updateSchedule(dto, id));
+    public ResponseEntity<MaintenanceScheduleResponse> update(@RequestBody UpdateMaintenanceScheduleRequest request, @PathVariable Integer id) {
+        return ResponseEntity.ok(maintenanceScheduleService.updateMaintenanceSchedule (request, id));
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<MaintenanceScheduleDTO> delete(@PathVariable Integer id) {
-        return ResponseEntity.ok().body(maintenanceScheduleServiceImpl.deleteSchedule(id));
+    public ResponseEntity<String> delete(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(maintenanceScheduleService.deleteMaintenanceSchedule(id));
     }
 }
