33722326f271ec9bb00f33438826236eca8587b3
rafael07-dev <deralato817@gmail.com>
2025-06-21 15:50:04 -0500

    get Monthly Maintenance by Maintenance schedule id

--- src/main/java/com/imat/oncomedica/inventory_management/infrastructure/repository/MonthlyMaintenanceRepository.java
+++ src/main/java/com/imat/oncomedica/inventory_management/infrastructure/repository/MonthlyMaintenanceRepository.java
@@ -3,5 +3,8 @@ package com.imat.oncomedica.inventory_management.infrastructure.repository;
 import com.imat.oncomedica.inventory_management.domain.entity.MonthlyMaintenance;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MonthlyMaintenanceRepository extends JpaRepository<MonthlyMaintenance, Integer> {
+    List<MonthlyMaintenance> findByMaintenanceSchedule_Id(Integer id);
 }
