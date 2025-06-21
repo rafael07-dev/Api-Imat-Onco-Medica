ce1d08ddc77bf324ccd2ad7b5883ed2b785f2928
rafael07-dev <deralato817@gmail.com>
2025-06-21 15:41:30 -0500

    add dto to create maintenance staff

---
+++ src/main/java/com/imat/oncomedica/inventory_management/application/dto/CreateMaintenanceStaffRequest.java
@@ -0,0 +1,12 @@
+package com.imat.oncomedica.inventory_management.application.dto;
+
+import lombok.Data;
+
+@Data
+public class CreateMaintenanceStaffRequest {
+
+    private String firstName;
+    private String lastName;
+    private String occupation;
+    private boolean availability;
+}
