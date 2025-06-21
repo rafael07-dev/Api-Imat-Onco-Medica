85ecb5ba02a1ede8e85afd82cbaa2da21246770f
rafael07-dev <deralato817@gmail.com>
2025-06-21 15:51:47 -0500

    add CRUD methods

--- src/main/java/com/imat/oncomedica/inventory_management/domain/service/MaintenanceScheduleService.java
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/service/MaintenanceScheduleService.java
@@ -1,4 +1,15 @@
 package com.imat.oncomedica.inventory_management.domain.service;
 
+import com.imat.oncomedica.inventory_management.application.dto.CreateMaintenanceScheduleRequest;
+import com.imat.oncomedica.inventory_management.application.dto.MaintenanceScheduleResponse;
+import com.imat.oncomedica.inventory_management.application.dto.UpdateMaintenanceScheduleRequest;
+
+import java.util.List;
+
 public interface MaintenanceScheduleService {
+    List<MaintenanceScheduleResponse> getAllMaintenanceSchedules();
+    MaintenanceScheduleResponse getMaintenanceScheduleById(Integer id);
+    MaintenanceScheduleResponse updateMaintenanceSchedule(UpdateMaintenanceScheduleRequest maintenanceSchedule, Integer id);
+    MaintenanceScheduleResponse createMaintenanceSchedule(CreateMaintenanceScheduleRequest createMaintenanceSchedule);
+    String deleteMaintenanceSchedule(Integer id);
 }
