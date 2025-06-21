3a40f2fab908090d949729ab516a500043beb41f
rafael07-dev <deralato817@gmail.com>
2025-06-21 15:52:09 -0500

    add MaintenanceStaffException

---
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/exception/MaintenanceStaffNotFound.java
@@ -0,0 +1,11 @@
+package com.imat.oncomedica.inventory_management.domain.exception;
+
+public class MaintenanceStaffNotFound extends RuntimeException{
+    public MaintenanceStaffNotFound() {
+        super("Maintenance Staff Not Found");
+    }
+
+    public MaintenanceStaffNotFound(Integer id) {
+        super("Maintenance Staff Not Found with id " + id);
+    }
+}
