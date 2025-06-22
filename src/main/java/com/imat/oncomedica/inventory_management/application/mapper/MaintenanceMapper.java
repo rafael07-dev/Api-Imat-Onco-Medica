e100f2437cba28c5a81881006a583483316d4f7b
rafael07-dev <deralato817@gmail.com>
2025-06-22 11:27:06 -0500

    update methods to mapping objects

--- src/main/java/com/imat/oncomedica/inventory_management/application/mapper/MaintenanceMapper.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/mapper/MaintenanceMapper.java
@@ -1,20 +1,53 @@
 package com.imat.oncomedica.inventory_management.application.mapper;
 
+import com.imat.oncomedica.inventory_management.application.dto.CreateMaintenanceRequest;
+import com.imat.oncomedica.inventory_management.application.dto.MaintenanceResponse;
+import com.imat.oncomedica.inventory_management.domain.entity.Equipment;
 import com.imat.oncomedica.inventory_management.domain.entity.Maintenance;
 import com.imat.oncomedica.inventory_management.application.dto.MaintenanceDTO;
+import com.imat.oncomedica.inventory_management.domain.entity.MaintenanceStaff;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface MaintenanceMapper {
 
-    @Mapping(source = "maintenance.equipment.id", target = "equipmentId")
-    @Mapping(source = "maintenance.maintenanceStaff.id", target = "maintenanceStaffId")
-    MaintenanceDTO toMaintenanceDTO(Maintenance maintenance);
+    default MaintenanceResponse toMaintenanceResponse(Maintenance maintenance){
 
-    //@Mapping(source = "maintenanceDTO.equipmentId", target = "equipment")
-    Maintenance toMaintenance(MaintenanceDTO maintenanceDTO);
+        return new MaintenanceResponse(
+                maintenance.getId(),
+                maintenance.getEquipment().getInventoryCode(),
+                maintenance.getMaintenanceStaff().getFirstName() + maintenance.getMaintenanceStaff().getLastName(),
+                maintenance.getMaintenanceStaff().getMaintenanceCompleted(),
+                maintenance.getMaintenanceStaff().getOccupation(),
+                maintenance.getScheduledDate(),
+                maintenance.getStartDate(),
+                maintenance.getStartTime(),
+                maintenance.getDeliveryDate(),
+                maintenance.getTimeUsed(),
+                maintenance.getRegistrationDate(),
+                maintenance.getDateOfCompletion(),
+                maintenance.getObservations()
+        );
+    };
 
-    List<MaintenanceDTO> toMaintenanceDtoList(List<Maintenance> maintenances);
+    default Maintenance toMaintenance(CreateMaintenanceRequest request, Equipment equipment, MaintenanceStaff maintenanceStaff){
+
+        return new Maintenance(
+                null,
+                equipment,
+                maintenanceStaff,
+                request.getScheduledDate(),
+                request.getStartDate(),
+                null,
+                request.getDeliveryDate(),
+                null,
+                request.getRegistrationDate(),
+                null,
+                request.getTypeMaintenance(),
+                request.getObservations()
+        );
+    };
+
+    List<MaintenanceDTO> toMaintenanceResponseList(List<Maintenance> maintenances);
 }
