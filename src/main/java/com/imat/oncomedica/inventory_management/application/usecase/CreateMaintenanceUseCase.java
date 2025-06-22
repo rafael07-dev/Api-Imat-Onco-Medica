37e16a22aa88ba9baa5158c069c35f7e9d1163d0
rafael07-dev <deralato817@gmail.com>
2025-06-22 11:28:44 -0500

    add maintenace use case

---
+++ src/main/java/com/imat/oncomedica/inventory_management/application/usecase/CreateMaintenanceUseCase.java
@@ -0,0 +1,45 @@
+package com.imat.oncomedica.inventory_management.application.usecase;
+
+import com.imat.oncomedica.inventory_management.application.dto.CreateMaintenanceRequest;
+import com.imat.oncomedica.inventory_management.application.dto.MaintenanceResponse;
+import com.imat.oncomedica.inventory_management.application.mapper.MaintenanceMapper;
+import com.imat.oncomedica.inventory_management.domain.exception.EquipmentNotFoundException;
+import com.imat.oncomedica.inventory_management.domain.exception.MaintenanceStaffNotFound;
+import com.imat.oncomedica.inventory_management.domain.service.NotificationService;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.EquipmentRepository;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceRepository;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceStaffRepository;
+
+public class CreateMaintenanceUseCase {
+
+    private final MaintenanceRepository maintenanceRepository;
+    private final EquipmentRepository equipmentRepository;
+    private final MaintenanceStaffRepository maintenanceStaffRepository;
+    private final MaintenanceMapper maintenanceMapper;
+    private final NotificationService notificationService;
+
+    public CreateMaintenanceUseCase(MaintenanceRepository maintenanceRepository, EquipmentRepository equipmentRepository, MaintenanceStaffRepository maintenanceStaffRepository, MaintenanceMapper maintenanceMapper, NotificationService notificationService) {
+        this.maintenanceRepository = maintenanceRepository;
+        this.equipmentRepository = equipmentRepository;
+        this.maintenanceStaffRepository = maintenanceStaffRepository;
+        this.maintenanceMapper = maintenanceMapper;
+        this.notificationService = notificationService;
+    }
+
+
+    public MaintenanceResponse execute(CreateMaintenanceRequest request) {
+        var equipment = equipmentRepository.findById(request.getEquipmentId())
+                .orElseThrow(() -> new EquipmentNotFoundException(request.getEquipmentId()));
+
+        var staff = maintenanceStaffRepository.findById(request.getStaffId())
+                .orElseThrow(() -> new MaintenanceStaffNotFound(request.getEquipmentId()));
+
+        var maintenace = maintenanceMapper.toMaintenance(request, equipment, staff);
+
+        var maintenanceResponse = maintenanceRepository.save(maintenace);
+
+        notificationService.notifyMaintenanceAssigned(maintenanceResponse);
+
+        return maintenanceMapper.toMaintenanceResponse(maintenanceResponse);
+    }
+}
