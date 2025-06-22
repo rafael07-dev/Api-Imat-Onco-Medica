a9ca8b0de92cd2b0ecc4d6897df396c2ebdd4233
rafael07-dev <deralato817@gmail.com>
2025-06-22 11:30:18 -0500

    add interface MaintenaceService

---
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/service/MaintenanceService.java
@@ -0,0 +1,16 @@
+package com.imat.oncomedica.inventory_management.domain.service;
+
+import com.imat.oncomedica.inventory_management.application.dto.CreateMaintenanceRequest;
+import com.imat.oncomedica.inventory_management.application.dto.MaintenanceResponse;
+import java.util.List;
+
+public interface MaintenanceService {
+
+    List<MaintenanceResponse> getAllMaintenances();
+    MaintenanceResponse getMaintenanceById(Integer id);
+    List<MaintenanceResponse> getMaintenanceByEquipmentID(Integer equipmentID);
+    List<MaintenanceResponse> getEquipmentByStaffId (Integer staffId);
+    MaintenanceResponse saveMaintenance(CreateMaintenanceRequest maintenance);
+    MaintenanceResponse updateMaintenance(CreateMaintenanceRequest maintenance, Integer id);
+    String deleteMaintenance(Integer id);
+}
