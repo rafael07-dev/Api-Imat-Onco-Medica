7cfd2bb3142d7c35c9e36e6066e48e189a4a46fa
rafael07-dev <deralato817@gmail.com>
2025-06-22 11:29:28 -0500

    add bean to container spring

---
+++ src/main/java/com/imat/oncomedica/inventory_management/config/CreateMaintenanceUseCaseConfig.java
@@ -0,0 +1,33 @@
+package com.imat.oncomedica.inventory_management.config;
+
+import com.imat.oncomedica.inventory_management.application.mapper.MaintenanceMapper;
+import com.imat.oncomedica.inventory_management.application.usecase.CreateMaintenanceUseCase;
+import com.imat.oncomedica.inventory_management.domain.service.NotificationService;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.EquipmentRepository;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceRepository;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceStaffRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CreateMaintenanceUseCaseConfig {
+
+    @Bean
+    public CreateMaintenanceUseCase createMaintenanceUseCase(
+            MaintenanceRepository maintenanceRepository,
+            EquipmentRepository equipmentRepository,
+            MaintenanceStaffRepository maintenanceStaffRepository,
+            MaintenanceMapper maintenanceMapper,
+            NotificationService notificationService
+
+    ) {
+        return new CreateMaintenanceUseCase(
+                maintenanceRepository,
+                equipmentRepository,
+                maintenanceStaffRepository,
+                maintenanceMapper,
+                notificationService
+
+        );
+    }
+}
