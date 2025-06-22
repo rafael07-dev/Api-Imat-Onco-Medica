2b157b14ca79e8cc74d944016c9e6377a8c6f012
rafael07-dev <deralato817@gmail.com>
2025-06-22 11:26:26 -0500

    add maintenance response

---
+++ src/main/java/com/imat/oncomedica/inventory_management/application/dto/MaintenanceResponse.java
@@ -0,0 +1,29 @@
+package com.imat.oncomedica.inventory_management.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class MaintenanceResponse {
+
+    private Long id;
+    private String inventoryCode;
+
+
+    private String maintenanceStaffName;
+    private Integer maintenancesNumber;
+    private String activity;
+
+    private Date scheduledDate;
+    private Date startDate;
+    private Date startTime;
+    private Date deliveryDate;
+    private Date timeUsed;
+    private Date registrationDate;
+    private Date dateOfCompletion;
+
+    private String observations;
+}
