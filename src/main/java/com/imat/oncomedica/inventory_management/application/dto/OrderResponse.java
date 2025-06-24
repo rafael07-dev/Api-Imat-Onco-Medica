559c02d9eb75e62ff63325dd0934a7ed4da0111b
rafael07-dev <deralato817@gmail.com>
2025-06-24 10:45:05 -0500

    add new dto order response

---
+++ src/main/java/com/imat/oncomedica/inventory_management/application/dto/OrderResponse.java
@@ -0,0 +1,18 @@
+package com.imat.oncomedica.inventory_management.application.dto;
+
+import com.imat.oncomedica.inventory_management.domain.entity.Equipment;
+import com.imat.oncomedica.inventory_management.domain.entity.MaintenanceStaff;
+import com.imat.oncomedica.inventory_management.domain.entity.OrderStatus;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Data
+public class OrderResponse {
+
+    private Integer id;
+    private LocalDateTime creationDate;
+    private MaintenanceStaff maintenanceStaff;
+    private Equipment equipment;
+    private OrderStatus status;
+    private String observation;
+}
