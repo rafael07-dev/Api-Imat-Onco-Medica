c71cdad7c2c5c68a9eb6f8892e5a41a763a22a67
rafael07-dev <deralato817@gmail.com>
2025-06-24 10:48:37 -0500

    add order service

---
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/service/OrderService.java
@@ -0,0 +1,11 @@
+package com.imat.oncomedica.inventory_management.domain.service;
+
+import com.imat.oncomedica.inventory_management.application.dto.OrderResponse;
+import java.util.List;
+
+public interface OrderService {
+
+    List<OrderResponse> getAllOrders();
+    OrderResponse getOrderById(Integer id);
+    List<OrderResponse> getOrdersByStaffId(Integer id);
+}
