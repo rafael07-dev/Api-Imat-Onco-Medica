a9879a923e49d531733d22ec1a56410ded4ee240
rafael07-dev <deralato817@gmail.com>
2025-06-24 10:54:55 -0500

    add update maintenance use case bean

---
+++ src/main/java/com/imat/oncomedica/inventory_management/config/UpdateMaintenanceUseCaseConfig.java
@@ -0,0 +1,28 @@
+package com.imat.oncomedica.inventory_management.config;
+
+import com.imat.oncomedica.inventory_management.application.mapper.MaintenanceMapper;
+import com.imat.oncomedica.inventory_management.application.usecase.UpdateMaintenanceUseCase;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.EquipmentRepository;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceRepository;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceStaffRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UpdateMaintenanceUseCaseConfig {
+
+    @Bean
+    public UpdateMaintenanceUseCase updateMaintenanceUseCase(
+            MaintenanceRepository maintenanceRepository,
+            MaintenanceMapper maintenanceMapper,
+            MaintenanceStaffRepository maintenanceStaffRepository,
+            EquipmentRepository equipmentRepository
+    ) {
+        return new UpdateMaintenanceUseCase(
+                maintenanceRepository,
+                maintenanceMapper,
+                maintenanceStaffRepository,
+                equipmentRepository
+        );
+    }
+}
