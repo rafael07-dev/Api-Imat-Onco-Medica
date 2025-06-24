5356673567b53fcd0161389aa794ac40fb715035
rafael07-dev <deralato817@gmail.com>
2025-06-24 10:56:33 -0500

    add create maintenance use case

--- src/main/java/com/imat/oncomedica/inventory_management/application/usecase/CreateMaintenanceUseCase.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/usecase/CreateMaintenanceUseCase.java
@@ -3,12 +3,16 @@ package com.imat.oncomedica.inventory_management.application.usecase;
 import com.imat.oncomedica.inventory_management.application.dto.CreateMaintenanceRequest;
 import com.imat.oncomedica.inventory_management.application.dto.MaintenanceResponse;
 import com.imat.oncomedica.inventory_management.application.mapper.MaintenanceMapper;
+import com.imat.oncomedica.inventory_management.domain.entity.Order;
 import com.imat.oncomedica.inventory_management.domain.exception.EquipmentNotFoundException;
 import com.imat.oncomedica.inventory_management.domain.exception.MaintenanceStaffNotFound;
 import com.imat.oncomedica.inventory_management.domain.service.NotificationService;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.EquipmentRepository;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceRepository;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceStaffRepository;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.OrderRepository;
+
+import java.time.LocalDateTime;
 
 public class CreateMaintenanceUseCase {
 
@@ -17,15 +21,16 @@ public class CreateMaintenanceUseCase {
     private final MaintenanceStaffRepository maintenanceStaffRepository;
     private final MaintenanceMapper maintenanceMapper;
     private final NotificationService notificationService;
+    private final OrderRepository orderRepository;
 
-    public CreateMaintenanceUseCase(MaintenanceRepository maintenanceRepository, EquipmentRepository equipmentRepository, MaintenanceStaffRepository maintenanceStaffRepository, MaintenanceMapper maintenanceMapper, NotificationService notificationService) {
+    public CreateMaintenanceUseCase(MaintenanceRepository maintenanceRepository, EquipmentRepository equipmentRepository, MaintenanceStaffRepository maintenanceStaffRepository, MaintenanceMapper maintenanceMapper, NotificationService notificationService, OrderRepository orderRepository) {
         this.maintenanceRepository = maintenanceRepository;
         this.equipmentRepository = equipmentRepository;
         this.maintenanceStaffRepository = maintenanceStaffRepository;
         this.maintenanceMapper = maintenanceMapper;
         this.notificationService = notificationService;
+        this.orderRepository = orderRepository;
     }
-
 
     public MaintenanceResponse execute(CreateMaintenanceRequest request) {
         var equipment = equipmentRepository.findById(request.getEquipmentId())
@@ -39,6 +44,14 @@ public class CreateMaintenanceUseCase {
         var maintenanceResponse = maintenanceRepository.save(maintenace);
 
         notificationService.notifyMaintenanceAssigned(maintenanceResponse);
+
+        Order order = new Order();
+        order.setCreationDate(LocalDateTime.now());
+        order.setEquipment(equipment);
+        order.setMaintenance(maintenace);
+        order.setMaintenanceStaff(staff);
+
+        orderRepository.save(order);
 
         return maintenanceMapper.toMaintenanceResponse(maintenanceResponse);
     }
