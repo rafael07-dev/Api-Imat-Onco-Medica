c7938cd4c9a4e4ce87ca2b56f6f8387b527f736d
rafael07-dev <deralato817@gmail.com>
2025-06-24 13:35:32 -0500

    ussing order repo and order pdf generator

--- src/main/java/com/imat/oncomedica/inventory_management/config/UpdateMaintenanceUseCaseConfig.java
+++ src/main/java/com/imat/oncomedica/inventory_management/config/UpdateMaintenanceUseCaseConfig.java
@@ -2,9 +2,11 @@ package com.imat.oncomedica.inventory_management.config;
 
 import com.imat.oncomedica.inventory_management.application.mapper.MaintenanceMapper;
 import com.imat.oncomedica.inventory_management.application.usecase.UpdateMaintenanceUseCase;
+import com.imat.oncomedica.inventory_management.infrastructure.report.OrderPdfGenerator;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.EquipmentRepository;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceRepository;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceStaffRepository;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.OrderRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,13 +18,17 @@ public class UpdateMaintenanceUseCaseConfig {
             MaintenanceRepository maintenanceRepository,
             MaintenanceMapper maintenanceMapper,
             MaintenanceStaffRepository maintenanceStaffRepository,
-            EquipmentRepository equipmentRepository
+            EquipmentRepository equipmentRepository,
+            OrderRepository orderRepository,
+            OrderPdfGenerator orderPdfGenerator
     ) {
         return new UpdateMaintenanceUseCase(
                 maintenanceRepository,
                 maintenanceMapper,
                 maintenanceStaffRepository,
-                equipmentRepository
+                equipmentRepository,
+                orderRepository,
+                orderPdfGenerator
         );
     }
 }
