88b16bdf6ba5814586217148fa17eefffec47d6b
rafael07-dev <deralato817@gmail.com>
2025-06-24 10:53:37 -0500

    add order repository

---
+++ src/main/java/com/imat/oncomedica/inventory_management/infrastructure/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.imat.oncomedica.inventory_management.infrastructure.repository;
+
+import com.imat.oncomedica.inventory_management.domain.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<Order, Integer> {
+
+    List<Order> findByMaintenanceStaff_Id(Integer id);
+    Order findByMaintenance_Id(Integer id);
+}
