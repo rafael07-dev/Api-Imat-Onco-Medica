ee630dd47e58cfe9f619db6e6006bd5aa16a209a
rafael07-dev <deralato817@gmail.com>
2025-06-25 12:30:57 -0500

    refactor report pdf exception

--- src/main/java/com/imat/oncomedica/inventory_management/domain/exception/ReportPdfGeneratorException.java
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/exception/ReportPdfGeneratorException.java
@@ -2,7 +2,7 @@ package com.imat.oncomedica.inventory_management.domain.exception;
 
 public class ReportPdfGeneratorException extends RuntimeException{
 
-    public ReportPdfGeneratorException (){
-        super("Error generating pdf");
+    public ReportPdfGeneratorException (String message){
+        super(message);
     }
 }
