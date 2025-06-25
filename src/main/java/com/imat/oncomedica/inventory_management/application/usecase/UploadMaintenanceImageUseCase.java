887170ce16a1885b9d0a26e3507739bfe8e22f3b
rafael07-dev <deralato817@gmail.com>
2025-06-25 12:29:23 -0500

    add upload maintenance image use case

--- src/main/java/com/imat/oncomedica/inventory_management/application/usecase/UploadMaintenanceImageUseCase.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/usecase/UploadMaintenanceImageUseCase.java
@@ -1,2 +1,32 @@
-package com.imat.oncomedica.inventory_management.application.usecase;public class UploadMaintenanceImageUseCase {
+package com.imat.oncomedica.inventory_management.application.usecase;
+
+import com.imat.oncomedica.inventory_management.domain.exception.MaintenanceNotFoundException;
+import com.imat.oncomedica.inventory_management.domain.service.FileStorageService;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+@Component
+public class UploadMaintenanceImageUseCase {
+
+    private final FileStorageService fileStorageService;
+    private final MaintenanceRepository maintenanceRepository;
+
+    public UploadMaintenanceImageUseCase(FileStorageService fileStorageService, MaintenanceRepository maintenanceRepository) {
+        this.fileStorageService = fileStorageService;
+        this.maintenanceRepository = maintenanceRepository;
+    }
+
+    public String UploadMaintenanceImageUseCase(MultipartFile file, Integer maintenanceId) {
+        var url = fileStorageService.uploadMaintenanceImage(maintenanceId, file);
+
+        var maintenanceSaved = maintenanceRepository.findById(maintenanceId)
+                .orElseThrow(() -> new MaintenanceNotFoundException(maintenanceId));
+
+        maintenanceSaved.setEvidenceImg(url);
+
+        maintenanceRepository.save(maintenanceSaved);
+
+        return url;
+    }
 }
