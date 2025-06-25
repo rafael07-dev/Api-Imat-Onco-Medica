134219ab7e349244d6f6c8d9c6db1a80aa8f48b7
rafael07-dev <deralato817@gmail.com>
2025-06-25 12:27:52 -0500

    completing maintenance use case

--- src/main/java/com/imat/oncomedica/inventory_management/application/usecase/CompleteMaintenanceUseCase.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/usecase/CompleteMaintenanceUseCase.java
@@ -3,11 +3,13 @@ package com.imat.oncomedica.inventory_management.application.usecase;
 import com.imat.oncomedica.inventory_management.application.dto.MaintenanceResponse;
 import com.imat.oncomedica.inventory_management.application.dto.UpdateMaintenanceRequest;
 import com.imat.oncomedica.inventory_management.application.mapper.MaintenanceMapper;
-import com.imat.oncomedica.inventory_management.domain.exception.EquipmentNotFoundException;
+import com.imat.oncomedica.inventory_management.domain.entity.OrderStatus;
 import com.imat.oncomedica.inventory_management.domain.exception.MaintenanceStaffNotFound;
+import com.imat.oncomedica.inventory_management.infrastructure.report.OrderPdfGenerator;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.EquipmentRepository;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceRepository;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceStaffRepository;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.OrderRepository;
 
 public class CompleteMaintenanceUseCase {
 
@@ -15,27 +17,42 @@ public class CompleteMaintenanceUseCase {
     private final MaintenanceRepository maintenanceRepository;
     private final MaintenanceMapper maintenanceMapper;
     private final EquipmentRepository equipmentRepository;
+    private final OrderPdfGenerator orderPdfGenerator;
+    private final OrderRepository orderRepository;
 
-    public CompleteMaintenanceUseCase(MaintenanceStaffRepository maintenanceStaffRepository, MaintenanceRepository maintenanceRepository, MaintenanceMapper maintenanceMapper, EquipmentRepository equipmentRepository) {
+    public CompleteMaintenanceUseCase(MaintenanceStaffRepository maintenanceStaffRepository, MaintenanceRepository maintenanceRepository, MaintenanceMapper maintenanceMapper, EquipmentRepository equipmentRepository, OrderPdfGenerator orderPdfGenerator, OrderRepository orderRepository) {
         this.maintenanceStaffRepository = maintenanceStaffRepository;
         this.maintenanceRepository = maintenanceRepository;
         this.maintenanceMapper = maintenanceMapper;
         this.equipmentRepository = equipmentRepository;
+        this.orderPdfGenerator = orderPdfGenerator;
+        this.orderRepository = orderRepository;
     }
 
-    public MaintenanceResponse execute(UpdateMaintenanceRequest request, Integer staffId, Integer maintenanceId) {
-        var staff = maintenanceStaffRepository.findById(staffId)
-                .orElseThrow(() -> new MaintenanceStaffNotFound(staffId));
+    public MaintenanceResponse execute(UpdateMaintenanceRequest request, Integer maintenanceId) {
 
-        var maintenance = maintenanceRepository.findById(maintenanceId)
+        var maintenanceEntity = maintenanceRepository.findById(maintenanceId)
                 .orElseThrow(() -> new MaintenanceStaffNotFound(maintenanceId));
 
-        var equipment = equipmentRepository.findById(maintenance.getEquipment().getId())
-                .orElseThrow(() -> new EquipmentNotFoundException(maintenance.getEquipment().getId()));
+        maintenanceEntity.setDone(true);
+        maintenanceEntity.setStaffObservations(request.getStaffObservations());
+        maintenanceEntity.setDeliveryTime(request.getDeliveryTime());
+        maintenanceEntity.setStartTime(request.getStartTime());
+        maintenanceEntity.setTimeUsed(request.getTimeUsed());
+        maintenanceEntity.setDateOfCompletion( request.getDateOfCompletion());
 
-        var maintenanceRequest = maintenanceMapper.updateMaintenance(request, equipment, staff);
+        var maintenanceSaved = maintenanceRepository.save(maintenanceEntity);
 
 
+        var order = orderRepository.findByMaintenance_Id(maintenanceId);
+
+        order.setStatus(OrderStatus.COMPLETED);
+
+        var orderSaved = orderRepository.save(order);
+
+        orderPdfGenerator.generateOderReport(orderSaved);
+
+        return maintenanceMapper.toMaintenanceResponse(maintenanceSaved);
 
     }
 }
