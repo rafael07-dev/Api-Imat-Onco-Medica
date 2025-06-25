bd9eda15a1fb2b48848f620ad82afdef1af477de
rafael07-dev <deralato817@gmail.com>
2025-06-25 12:23:11 -0500

    jasper dependencies

---
+++ src/main/java/com/imat/oncomedica/inventory_management/application/usecase/UploadMaintenanceImageUseCase.java
@@ -0,0 +1,2 @@
+package com.imat.oncomedica.inventory_management.application.usecase;public class UploadMaintenanceImageUseCase {
+}
---
+++ src/main/java/com/imat/oncomedica/inventory_management/config/CompleteMaintenanceUseCaseConfig.java
@@ -0,0 +1,2 @@
+package com.imat.oncomedica.inventory_management.config;public class CompleteMaintenanceUseCaseConfig {
+}
