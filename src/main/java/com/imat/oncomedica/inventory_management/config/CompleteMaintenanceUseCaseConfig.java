385ba8f4c631d51721e8b4bd69f63ecf0e538a2c
rafael07-dev <deralato817@gmail.com>
2025-06-25 12:29:44 -0500

    add bean to complete maintenance

--- src/main/java/com/imat/oncomedica/inventory_management/config/CompleteMaintenanceUseCaseConfig.java
+++ src/main/java/com/imat/oncomedica/inventory_management/config/CompleteMaintenanceUseCaseConfig.java
@@ -1,2 +1,33 @@
-package com.imat.oncomedica.inventory_management.config;public class CompleteMaintenanceUseCaseConfig {
+package com.imat.oncomedica.inventory_management.config;
+
+import com.imat.oncomedica.inventory_management.application.mapper.MaintenanceMapper;
+import com.imat.oncomedica.inventory_management.application.usecase.CompleteMaintenanceUseCase;
+import com.imat.oncomedica.inventory_management.infrastructure.report.OrderPdfGenerator;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.EquipmentRepository;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceRepository;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceStaffRepository;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.OrderRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CompleteMaintenanceUseCaseConfig {
+
+    @Bean
+    public CompleteMaintenanceUseCase completeMaintenanceUseCase (
+            MaintenanceStaffRepository maintenanceStaffRepository,
+            MaintenanceRepository maintenanceRepository,
+            MaintenanceMapper maintenanceMapper,
+            EquipmentRepository equipmentRepository,
+            OrderPdfGenerator orderPdfGenerator,
+            OrderRepository orderRepository
+    ) {
+        return new CompleteMaintenanceUseCase(
+                maintenanceStaffRepository,
+                maintenanceRepository,
+                maintenanceMapper,
+                equipmentRepository,
+                orderPdfGenerator,
+                orderRepository);
+    }
 }
