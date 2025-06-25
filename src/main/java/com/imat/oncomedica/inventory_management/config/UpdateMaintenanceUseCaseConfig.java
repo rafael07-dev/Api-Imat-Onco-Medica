0f499ce3188704f08693f9348ccb0142d00a9049
rafael07-dev <deralato817@gmail.com>
2025-06-25 12:25:06 -0500

    add bean to update maintenance use case

--- src/main/java/com/imat/oncomedica/inventory_management/config/UpdateMaintenanceUseCaseConfig.java
+++ src/main/java/com/imat/oncomedica/inventory_management/config/UpdateMaintenanceUseCaseConfig.java
@@ -2,11 +2,9 @@ package com.imat.oncomedica.inventory_management.config;
 
 import com.imat.oncomedica.inventory_management.application.mapper.MaintenanceMapper;
 import com.imat.oncomedica.inventory_management.application.usecase.UpdateMaintenanceUseCase;
-import com.imat.oncomedica.inventory_management.infrastructure.report.OrderPdfGenerator;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.EquipmentRepository;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceRepository;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceStaffRepository;
-import com.imat.oncomedica.inventory_management.infrastructure.repository.OrderRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,17 +16,13 @@ public class UpdateMaintenanceUseCaseConfig {
             MaintenanceRepository maintenanceRepository,
             MaintenanceMapper maintenanceMapper,
             MaintenanceStaffRepository maintenanceStaffRepository,
-            EquipmentRepository equipmentRepository,
-            OrderRepository orderRepository,
-            OrderPdfGenerator orderPdfGenerator
+            EquipmentRepository equipmentRepository
     ) {
         return new UpdateMaintenanceUseCase(
                 maintenanceRepository,
                 maintenanceMapper,
                 maintenanceStaffRepository,
-                equipmentRepository,
-                orderRepository,
-                orderPdfGenerator
+                equipmentRepository
         );
     }
 }
