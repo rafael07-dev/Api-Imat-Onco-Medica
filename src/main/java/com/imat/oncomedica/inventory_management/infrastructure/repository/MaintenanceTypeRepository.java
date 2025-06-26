bf6f2dfebbfe222c985d765f49d814ad6a190484
rafael07-dev <deralato817@gmail.com>
2025-06-26 18:49:47 -0500

    add maintenanceType repo

--- src/main/java/com/imat/oncomedica/inventory_management/infrastructure/repository/MaintenanceTypeRepository.java
+++ src/main/java/com/imat/oncomedica/inventory_management/infrastructure/repository/MaintenanceTypeRepository.java
@@ -1,2 +1,10 @@
-package com.imat.oncomedica.inventory_management.infrastructure.repository;public interface MaintenanceTypeRepository {
+package com.imat.oncomedica.inventory_management.infrastructure.repository;
+
+import com.imat.oncomedica.inventory_management.domain.entity.MaintenanceType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MaintenanceTypeRepository extends JpaRepository<MaintenanceType, Integer> {
+    MaintenanceType findByName(String name);
 }
