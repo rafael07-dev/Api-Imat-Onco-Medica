b2949ff4dbf39a7487bd36780ed75db757bd80fb
rafael07-dev <deralato817@gmail.com>
2025-06-26 09:05:10 -0500

    delete unncesary dtos

--- src/main/java/com/imat/oncomedica/inventory_management/application/dto/MaintenanceHistoryDTO.java
+++
@@ -1,8 +0,0 @@
-package com.imat.oncomedica.inventory_management.application.dto;
-
-public class MaintenanceHistoryDTO {
-
-    private Integer id;
-    private Integer shedule_id;
-    private Integer maintenance_id;
-}
