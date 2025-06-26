a1c2a1a97c42dac8714654ef6d89a4848bc311c3
rafael07-dev <deralato817@gmail.com>
2025-06-26 10:32:18 -0500

    add use case to bean

--- src/main/java/com/imat/oncomedica/inventory_management/config/CreateMaintenanceUseCaseConfig.java
+++ src/main/java/com/imat/oncomedica/inventory_management/config/CreateMaintenanceUseCaseConfig.java
@@ -2,11 +2,11 @@ package com.imat.oncomedica.inventory_management.config;
 
 import com.imat.oncomedica.inventory_management.application.mapper.MaintenanceMapper;
 import com.imat.oncomedica.inventory_management.application.usecase.CreateMaintenanceUseCase;
+import com.imat.oncomedica.inventory_management.application.usecase.CreateOrderUseCase;
 import com.imat.oncomedica.inventory_management.domain.service.NotificationService;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.EquipmentRepository;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceRepository;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceStaffRepository;
-import com.imat.oncomedica.inventory_management.infrastructure.repository.OrderRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,14 +20,13 @@ public class CreateMaintenanceUseCaseConfig {
             MaintenanceStaffRepository maintenanceStaffRepository,
             MaintenanceMapper maintenanceMapper,
             NotificationService notificationService,
-            OrderRepository orderRepository) {
+            CreateOrderUseCase createOrderUseCase) {
         return new CreateMaintenanceUseCase(
                 maintenanceRepository,
                 equipmentRepository,
                 maintenanceStaffRepository,
                 maintenanceMapper,
                 notificationService,
-                orderRepository
-        );
+                createOrderUseCase);
     }
 }
