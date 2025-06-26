1282d41793ce4b1b4955d57114f271d8504ab891
rafael07-dev <deralato817@gmail.com>
2025-06-26 18:46:37 -0500

    add montly maintenance mapper

--- src/main/java/com/imat/oncomedica/inventory_management/application/mapper/MonthlyMaintenanceMapper.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/mapper/MonthlyMaintenanceMapper.java
@@ -1,8 +1,11 @@
 package com.imat.oncomedica.inventory_management.application.mapper;
 
+import com.imat.oncomedica.inventory_management.application.dto.CreateMonthlyMaintenanceRequest;
+import com.imat.oncomedica.inventory_management.domain.entity.MonthlyMaintenance;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface MonthlyMaintenanceMapper {
 
+    MonthlyMaintenance toMonthlyMaintenance(CreateMonthlyMaintenanceRequest request);
 }
