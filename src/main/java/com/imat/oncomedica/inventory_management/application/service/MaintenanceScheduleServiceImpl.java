998454ed27d5b17668cc9035dd0e341de61cbffc
rafael07-dev <deralato817@gmail.com>
2025-06-26 18:48:10 -0500

    saving diferents maintenances types and quantities

--- src/main/java/com/imat/oncomedica/inventory_management/application/service/MaintenanceScheduleServiceImpl.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/service/MaintenanceScheduleServiceImpl.java
@@ -1,9 +1,11 @@
 package com.imat.oncomedica.inventory_management.application.service;
 
 import com.imat.oncomedica.inventory_management.application.dto.*;
+import com.imat.oncomedica.inventory_management.application.mapper.MonthlyMaintenanceTypeMapper;
 import com.imat.oncomedica.inventory_management.domain.entity.MaintenanceSchedule;
 import com.imat.oncomedica.inventory_management.domain.entity.MonthlyMaintenance;
 import com.imat.oncomedica.inventory_management.application.mapper.MaintenanceScheduleMapper;
+import com.imat.oncomedica.inventory_management.domain.entity.MonthlyMaintenanceType;
 import com.imat.oncomedica.inventory_management.domain.exception.EquipmentNotFoundException;
 import com.imat.oncomedica.inventory_management.domain.exception.MaintenanceScheduleNotFoundException;
 import com.imat.oncomedica.inventory_management.domain.exception.MaintenanceStaffNotFound;
@@ -11,7 +13,7 @@ import com.imat.oncomedica.inventory_management.domain.service.MaintenanceSchedu
 import com.imat.oncomedica.inventory_management.infrastructure.repository.EquipmentRepository;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceScheduleRepository;
 import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceStaffRepository;
-import com.imat.oncomedica.inventory_management.infrastructure.repository.MonthlyMaintenanceRepository;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceTypeRepository;
 import org.springframework.stereotype.Service;
 import java.util.*;
 
@@ -21,8 +23,9 @@ public class MaintenanceScheduleServiceImpl implements MaintenanceScheduleServic
     private final EquipmentRepository equipmentRepository;
     private final MaintenanceScheduleRepository maintenanceScheduleRepository;
     private final MaintenanceScheduleMapper maintenanceScheduleMapper;
-    private final MonthlyMaintenanceRepository monthlyMaintenanceRepository;
     private final MaintenanceStaffRepository maintenanceStaffRepository;
+    private final MonthlyMaintenanceTypeMapper monthlyMaintenanceTypeMapper;
+    private final MaintenanceTypeRepository maintenanceTypeRepository;
     /*
     * An array of months to compare
     */
@@ -32,13 +35,15 @@ public class MaintenanceScheduleServiceImpl implements MaintenanceScheduleServic
             "September", "October", "November", "December"
     };
 
-    public MaintenanceScheduleServiceImpl(EquipmentRepository equipmentRepository, MaintenanceScheduleRepository maintenanceScheduleRepository, MaintenanceScheduleMapper maintenanceScheduleMapper, MonthlyMaintenanceRepository monthlyMaintenanceRepository, MaintenanceStaffRepository maintenanceStaffRepository) {
+    public MaintenanceScheduleServiceImpl(EquipmentRepository equipmentRepository, MaintenanceScheduleRepository maintenanceScheduleRepository, MaintenanceScheduleMapper maintenanceScheduleMapper, MaintenanceStaffRepository maintenanceStaffRepository, MonthlyMaintenanceTypeMapper monthlyMaintenanceTypeMapper, MaintenanceTypeRepository maintenanceTypeRepository) {
         this.equipmentRepository = equipmentRepository;
         this.maintenanceScheduleRepository = maintenanceScheduleRepository;
         this.maintenanceScheduleMapper = maintenanceScheduleMapper;
-        this.monthlyMaintenanceRepository = monthlyMaintenanceRepository;
         this.maintenanceStaffRepository = maintenanceStaffRepository;
+        this.monthlyMaintenanceTypeMapper = monthlyMaintenanceTypeMapper;
+        this.maintenanceTypeRepository = maintenanceTypeRepository;
     }
+
 
     @Override
     public List<MaintenanceScheduleResponse> getAllMaintenanceSchedules() {
@@ -76,19 +81,36 @@ public class MaintenanceScheduleServiceImpl implements MaintenanceScheduleServic
         maintenanceSchedule.setResponsible(maintenanceStaff);
         maintenanceSchedule.setMonthlyMaintenances(new ArrayList<>());
 
-        var maintenanceList = maintenanceSchedule.getMonthlyMaintenances();
+        List<MonthlyMaintenance> monthlyMaintenances = new ArrayList<>();
 
         request.getMonths()
                 .stream()
-                .forEach(month -> {
-                    var mm = new MonthlyMaintenance();
+                .forEach(m -> {
+                    var monthlyMaintenance = new MonthlyMaintenance();
+                    monthlyMaintenance.setMonth(m.getMonth());
+                    monthlyMaintenance.setYear(m.getYear());
+                    monthlyMaintenance.setMaintenanceSchedule(maintenanceSchedule);
 
-                    mm.setMonth(month.getMonth());
-                    mm.setMaintenanceType(month.getMaintenanceType());
-                    mm.setMaintenanceSchedule(maintenanceSchedule);
+                    var monthlyMaintenanceType = new ArrayList<MonthlyMaintenanceType>();
+                    m.getMaintenanceType()
+                            .stream()
+                            .forEach(mt -> {
+                                var mmt = new MonthlyMaintenanceType();
 
-                    maintenanceList.add(mm);
+                                var name = mt.getMaintenanceType().getName();
+                                var maintenanceType = maintenanceTypeRepository.findByName(name);
+
+                                mmt.setMaintenanceType(maintenanceType);
+                                mmt.setQuantity(mt.getQuantity());
+
+                                mmt.setMonthlyMaintenance(monthlyMaintenance);
+                                monthlyMaintenanceType.add(mmt);
+                            });
+
+                    monthlyMaintenance.setMaintenanceTypes(monthlyMaintenanceType);
+                    monthlyMaintenances.add(monthlyMaintenance);
                 });
+        maintenanceSchedule.setMonthlyMaintenances(monthlyMaintenances);
 
         var maintenanceSaved = maintenanceScheduleRepository.save(maintenanceSchedule);
 
