69edcb09a5b84052f58b70e32599d3f8e78880eb
rafael07-dev <deralato817@gmail.com>
2025-06-27 10:13:06 -0500

    add monthly maintenace respinse

--- src/main/java/com/imat/oncomedica/inventory_management/application/dto/maintenance/MonthlyMaintenanceResponse.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/dto/maintenance/MonthlyMaintenanceResponse.java
@@ -1,2 +1,14 @@
-package com.imat.oncomedica.inventory_management.application.dto.maintenance;public class MonthlyMaintenanceRespose {
+package com.imat.oncomedica.inventory_management.application.dto.maintenance;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class MonthlyMaintenanceResponse {
+
+    private Integer month;
+    private Integer year;
+
+    List<MonthlyMaintenanceTypeResponse> maintenances;
 }
