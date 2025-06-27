02c8cb0d79a57822b7011c2462b8cec6b4d64e9d
rafael07-dev <deralato817@gmail.com>
2025-06-27 17:26:20 -0500

    add maintenance type exception

--- src/main/java/com/imat/oncomedica/inventory_management/domain/exception/MaintenanceTypeNotFoundException.java
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/exception/MaintenanceTypeNotFoundException.java
@@ -1,2 +1,12 @@
-package com.imat.oncomedica.inventory_management.domain.exception;public class MaintenanceTypeNotFoundException {
+package com.imat.oncomedica.inventory_management.domain.exception;
+
+
+public class MaintenanceTypeNotFoundException extends RuntimeException{
+
+    public MaintenanceTypeNotFoundException(){
+        super("Maintenance Type Not Found");
+    }
+    public MaintenanceTypeNotFoundException(String message) {
+        super(message);
+    }
 }
