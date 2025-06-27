f4e6ba96687b200788fc027b52cf11f520136523
rafael07-dev <deralato817@gmail.com>
2025-06-27 10:03:04 -0500

    add contructors

--- src/main/java/com/imat/oncomedica/inventory_management/domain/entity/MonthlyMaintenanceType.java
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/entity/MonthlyMaintenanceType.java
@@ -2,20 +2,22 @@ package com.imat.oncomedica.inventory_management.domain.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MonthlyMaintenanceType {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @ManyToOne
-    @JoinColumn(name = "maintenance_type_id")
-    @JsonIgnore
-    private MaintenanceType maintenanceType;
+    @Enumerated(EnumType.STRING)
+    private MaintenanceTypeEnum maintenanceTypeEnum;
 
     @ManyToOne
     @JoinColumn(name = "monthly_maintenance_id")
