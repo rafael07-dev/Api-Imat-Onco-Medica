e20cba444a60da10bfa28450f4c00e336758c912
rafael07-dev <deralato817@gmail.com>
2025-06-27 10:18:44 -0500

    delete createMaintenanceSchedule method, migrated to use case

--- src/main/java/com/imat/oncomedica/inventory_management/domain/service/MaintenanceScheduleService.java
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/service/MaintenanceScheduleService.java
@@ -1,15 +1,12 @@
 package com.imat.oncomedica.inventory_management.domain.service;
 
-import com.imat.oncomedica.inventory_management.application.dto.CreateMaintenanceScheduleRequest;
-import com.imat.oncomedica.inventory_management.application.dto.MaintenanceScheduleResponse;
-import com.imat.oncomedica.inventory_management.application.dto.UpdateMaintenanceScheduleRequest;
-
+import com.imat.oncomedica.inventory_management.application.dto.schedule.MaintenanceScheduleResponse;
+import com.imat.oncomedica.inventory_management.application.dto.schedule.UpdateMaintenanceScheduleRequest;
 import java.util.List;
 
 public interface MaintenanceScheduleService {
     List<MaintenanceScheduleResponse> getAllMaintenanceSchedules();
     MaintenanceScheduleResponse getMaintenanceScheduleById(Integer id);
     MaintenanceScheduleResponse updateMaintenanceSchedule(UpdateMaintenanceScheduleRequest maintenanceSchedule, Integer id);
-    MaintenanceScheduleResponse createMaintenanceSchedule(CreateMaintenanceScheduleRequest createMaintenanceSchedule);
     String deleteMaintenanceSchedule(Integer id);
 }
