ed2f5646ff037d6d94cf0922f57a15eecd5078a5
rafael07-dev <deralato817@gmail.com>
2025-06-27 10:04:38 -0500

    add maintenance schedule fatory to buil objects

--- src/main/java/com/imat/oncomedica/inventory_management/domain/factory/MaintenanceScheduleFactory.java
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/factory/MaintenanceScheduleFactory.java
@@ -1,2 +1,21 @@
-package com.imat.oncomedica.inventory_management.domain.factory;public class MaintenanceScheduleFactory {
+package com.imat.oncomedica.inventory_management.domain.factory;
+
+import com.imat.oncomedica.inventory_management.domain.entity.Equipment;
+import com.imat.oncomedica.inventory_management.domain.entity.MaintenanceSchedule;
+import com.imat.oncomedica.inventory_management.domain.entity.MaintenanceStaff;
+import org.springframework.stereotype.Component;
+import java.util.ArrayList;
+
+@Component
+public class MaintenanceScheduleFactory {
+
+    public MaintenanceSchedule create(Equipment equipment, MaintenanceStaff staff) {
+        var maintenanceSchedule = new MaintenanceSchedule();
+
+        maintenanceSchedule.setEquipment(equipment);
+        maintenanceSchedule.setResponsible(staff);
+        maintenanceSchedule.setMonthlyMaintenances(new ArrayList<>());
+
+        return maintenanceSchedule;
+    }
 }
