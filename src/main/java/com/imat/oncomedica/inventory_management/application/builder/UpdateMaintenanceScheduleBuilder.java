ba0e66dec298038a4b9dfea05d8499c1d9c2b1c0
rafael07-dev <deralato817@gmail.com>
2025-07-01 09:39:05 -0500

    add UpdateMaintenanceScheduleBuilder to update MaintenanceSchedule

--- src/main/java/com/imat/oncomedica/inventory_management/application/builder/UpdateMaintenanceScheduleBuilder.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/builder/UpdateMaintenanceScheduleBuilder.java
@@ -1,2 +1,47 @@
-package com.imat.oncomedica.inventory_management.application.builder;public class UpdateMaintenanceScheduleBuilder {
+package com.imat.oncomedica.inventory_management.application.builder;
+
+import static com.imat.oncomedica.inventory_management.application.builder.util.MaintenanceScheduleBuilderHelper.*;
+import com.imat.oncomedica.inventory_management.application.dto.schedule.MaintenanceScheduleRequest;
+import com.imat.oncomedica.inventory_management.application.dto.schedule.UpdateMaintenanceScheduleRequest;
+import com.imat.oncomedica.inventory_management.domain.entity.MaintenanceSchedule;
+import com.imat.oncomedica.inventory_management.domain.entity.MonthlyMaintenance;
+import com.imat.oncomedica.inventory_management.domain.exception.MaintenanceScheduleNotFoundException;
+import com.imat.oncomedica.inventory_management.domain.exception.MonthlyMaintenanceNotFoundException;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceScheduleRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UpdateMaintenanceScheduleBuilder {
+
+    private final MaintenanceScheduleRepository maintenanceScheduleRepository;
+
+    public UpdateMaintenanceScheduleBuilder(MaintenanceScheduleRepository maintenanceScheduleRepository) {
+        this.maintenanceScheduleRepository = maintenanceScheduleRepository;
+    }
+
+    public MaintenanceSchedule build(MaintenanceScheduleRequest request, Integer id) {
+
+        var maintenanceSchedule = maintenanceScheduleRepository.findById(id)
+                .orElseThrow(MaintenanceScheduleNotFoundException::new);
+
+        if(!(request instanceof UpdateMaintenanceScheduleRequest)){
+            throw new RuntimeException("Invalid request");
+        }
+
+        if (request.getMonthlyMaintenances() == null || request.getMonthlyMaintenances().isEmpty()) {
+            throw new MonthlyMaintenanceNotFoundException("should have at least one maintenance");
+        }
+
+        List<MonthlyMaintenance> monthlyMaintenances = getMonthlyMaintenancesFromRequest(request.getMonthlyMaintenances());
+
+        associateMonthlyMaintenanceToSchedule(monthlyMaintenances, maintenanceSchedule);
+
+        maintenanceSchedule.getMonthlyMaintenances().clear();
+
+        maintenanceSchedule.getMonthlyMaintenances().addAll(monthlyMaintenances);
+
+        return maintenanceSchedule;
+    }
 }
