399f7a0e4f972debf01db1d6025c527d4b956c83
rafael07-dev <deralato817@gmail.com>
2025-07-01 09:30:27 -0500

    add UpdateMaintenaceScheduleUseCase to update MaintenanceSchedule

--- src/main/java/com/imat/oncomedica/inventory_management/application/usecase/UpdateMaintenanceScheduleUseCase.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/usecase/UpdateMaintenanceScheduleUseCase.java
@@ -1,2 +1,36 @@
-package com.imat.oncomedica.inventory_management.application.usecase;public class UpdateMaintenanceScheduleUseCase {
+package com.imat.oncomedica.inventory_management.application.usecase;
+
+import com.imat.oncomedica.inventory_management.application.builder.UpdateMaintenanceScheduleBuilder;
+import com.imat.oncomedica.inventory_management.application.dto.schedule.MaintenanceScheduleResponse;
+import com.imat.oncomedica.inventory_management.application.dto.schedule.UpdateMaintenanceScheduleRequest;
+import com.imat.oncomedica.inventory_management.application.mapper.MaintenanceScheduleMapper;
+import com.imat.oncomedica.inventory_management.domain.exception.MaintenanceScheduleNotFoundException;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceScheduleRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UpdateMaintenanceScheduleUseCase {
+
+    private final UpdateMaintenanceScheduleBuilder updateMaintenanceScheduleBuilder;
+    private final MaintenanceScheduleRepository maintenanceScheduleRepository;
+    private final MaintenanceScheduleMapper maintenanceScheduleMapper;
+
+    public UpdateMaintenanceScheduleUseCase(UpdateMaintenanceScheduleBuilder updateMaintenanceScheduleBuilder, MaintenanceScheduleRepository maintenanceScheduleRepository, MaintenanceScheduleMapper maintenanceScheduleMapper) {
+        this.updateMaintenanceScheduleBuilder = updateMaintenanceScheduleBuilder;
+        this.maintenanceScheduleRepository = maintenanceScheduleRepository;
+        this.maintenanceScheduleMapper = maintenanceScheduleMapper;
+    }
+
+
+    public MaintenanceScheduleResponse execute(UpdateMaintenanceScheduleRequest request, Integer id) {
+
+        if (!maintenanceScheduleRepository.existsById(id)) {
+            throw new MaintenanceScheduleNotFoundException(id);
+        }
+
+        var maintenanceSchedule = updateMaintenanceScheduleBuilder.build(request, id);
+
+        var maintenanceScheduleSaved = maintenanceScheduleRepository.save(maintenanceSchedule);
+        return maintenanceScheduleMapper.buildMaintenanceScheduleResponse(maintenanceScheduleSaved);
+    }
 }
