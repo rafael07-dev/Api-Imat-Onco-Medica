31b4d3ff58430f7aaa266a02705d527127fd526c
rafael07-dev <deralato817@gmail.com>
2025-07-01 09:29:30 -0500

    find maintenanceSchedule by equipment id

--- src/main/java/com/imat/oncomedica/inventory_management/infrastructure/repository/MaintenanceScheduleRepository.java
+++ src/main/java/com/imat/oncomedica/inventory_management/infrastructure/repository/MaintenanceScheduleRepository.java
@@ -2,7 +2,8 @@ package com.imat.oncomedica.inventory_management.infrastructure.repository;
 
 import com.imat.oncomedica.inventory_management.domain.entity.MaintenanceSchedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface MaintenanceScheduleRepository extends JpaRepository<MaintenanceSchedule, Integer> {
-    boolean findMaintenanceScheduleByEquipmentId(Integer equipmentId);
+    Optional<MaintenanceSchedule> findMaintenanceScheduleByEquipment_Id(Integer equipmentId);
 }
