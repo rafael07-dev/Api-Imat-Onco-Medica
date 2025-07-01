d8eee8a78c0fc9e8ee8dbee7c00c851951956889
rafael07-dev <deralato817@gmail.com>
2025-07-01 09:34:22 -0500

    add MaintenanceScheduelFactory to create MaintenanceSchedule entity

--- src/main/java/com/imat/oncomedica/inventory_management/application/factory/MaintenanceScheduleFactory.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/factory/MaintenanceScheduleFactory.java
@@ -1,2 +1,41 @@
-package com.imat.oncomedica.inventory_management.application.factory;public class MaintenanceScheduleFactory {
+package com.imat.oncomedica.inventory_management.application.factory;
+
+import com.imat.oncomedica.inventory_management.application.builder.util.MaintenanceScheduleBuilderHelper;
+import com.imat.oncomedica.inventory_management.application.dto.schedule.MaintenanceScheduleRequest;
+import com.imat.oncomedica.inventory_management.domain.entity.MaintenanceSchedule;
+import com.imat.oncomedica.inventory_management.domain.exception.EquipmentNotFoundException;
+import com.imat.oncomedica.inventory_management.domain.exception.MaintenanceStaffNotFound;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.EquipmentRepository;
+import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceStaffRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MaintenanceScheduleFactory {
+
+    private final EquipmentRepository equipmentRepository;
+    private final MaintenanceStaffRepository maintenanceStaffRepository;
+
+    public MaintenanceScheduleFactory(EquipmentRepository equipmentRepository, MaintenanceStaffRepository maintenanceStaffRepository) {
+        this.equipmentRepository = equipmentRepository;
+        this.maintenanceStaffRepository = maintenanceStaffRepository;
+    }
+
+    public MaintenanceSchedule buildMaintenanceSchedule(MaintenanceScheduleRequest request) {
+        MaintenanceSchedule maintenanceSchedule = new MaintenanceSchedule();
+
+        var equipment = equipmentRepository.findById(request.getEquipmentId())
+                .orElseThrow(() -> new EquipmentNotFoundException(request.getEquipmentId()));
+
+        var staff = maintenanceStaffRepository.findById(request.getMaintenanceStaffId())
+                .orElseThrow(() -> new MaintenanceStaffNotFound(request.getMaintenanceStaffId()));
+
+        maintenanceSchedule.setEquipment(equipment);
+        maintenanceSchedule.setResponsible(staff);
+
+        var monthlyMaintenances = MaintenanceScheduleBuilderHelper.getMonthlyMaintenancesFromRequest(request.getMonthlyMaintenances());
+
+        maintenanceSchedule.setMonthlyMaintenances(monthlyMaintenances);
+
+        return maintenanceSchedule;
+    }
 }
