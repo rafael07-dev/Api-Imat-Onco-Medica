3a029e80fc04f29fa083081c834f7fc5c8dde8af
rafael07-dev <deralato817@gmail.com>
2025-07-04 12:05:58 -0500

    mover entidad maintenance schedule a infra, y convertir modelo de domain en POJO

--- src/main/java/com/imat/oncomedica/inventory_management/domain/model/MaintenanceSchedule.java
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/model/MaintenanceSchedule.java
@@ -1,32 +1,20 @@
 package com.imat.oncomedica.inventory_management.domain.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.io.Serializable;
 import java.util.List;
 
-@Entity
-@Table(name = "cronograma")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MaintenanceSchedule implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-
-    @ManyToOne
-    @JoinColumn(name = "equipment_id")
     private Equipment equipment;
 
-    @ManyToOne
-    @JoinColumn(name = "responsible_id")
     private MaintenanceStaff responsible;
 
-    @OneToMany(mappedBy = "maintenanceSchedule", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<MonthlyMaintenance> monthlyMaintenances;
 
 }
---
+++ src/main/java/com/imat/oncomedica/inventory_management/infrastructure/persistence/entity/MaintenanceScheduleEntity.java
@@ -0,0 +1,34 @@
+package com.imat.oncomedica.inventory_management.infrastructure.persistence.entity;
+
+import com.imat.oncomedica.inventory_management.domain.model.MaintenanceStaff;
+import com.imat.oncomedica.inventory_management.domain.model.MonthlyMaintenance;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "cronograma")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MaintenanceScheduleEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "equipment_id")
+    private EquipmentEntity equipment;
+
+    @ManyToOne
+    @JoinColumn(name = "responsible_id")
+    private MaintenanceStaff responsible;
+
+    @OneToMany(mappedBy = "maintenanceSchedule", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<MonthlyMaintenance> monthlyMaintenances;
+
+}
