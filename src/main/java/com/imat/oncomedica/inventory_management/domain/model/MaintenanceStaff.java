d13af52ba1928c2028d858c0687b121e725c5f22
rafael07-dev <deralato817@gmail.com>
2025-07-10 17:47:18 -0500

    migrar entidad maintenanceStaff a infrastructure, para seguir principios de clean architecture

--- src/main/java/com/imat/oncomedica/inventory_management/domain/model/MaintenanceStaff.java
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/model/MaintenanceStaff.java
@@ -1,43 +1,31 @@
 package com.imat.oncomedica.inventory_management.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.util.List;
 
-@Entity
-@Table(name = "personal_mantenimiento")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class MaintenanceStaff {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "nombre", nullable = false)
     private String firstName;
 
-    @Column(name = "apellido", nullable = false)
     private String lastName;
 
     private String email;
 
     private String occupation;
 
-    @Column(nullable = false)
     private boolean availability; // "available" or "busy"
 
-    @Column(name = "mantenimientos_completados")
     private Integer maintenanceCompleted;
 
     private String signaturePath;
 
-    @OneToMany(mappedBy = "maintenanceStaff", cascade = CascadeType.ALL)
-    @JsonIgnore
     private List<Order> orderList;
 
 }
---
+++ src/main/java/com/imat/oncomedica/inventory_management/infrastructure/persistence/entity/MaintenanceStaffEntity.java
@@ -0,0 +1,44 @@
+package com.imat.oncomedica.inventory_management.infrastructure.persistence.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.imat.oncomedica.inventory_management.domain.model.Order;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "personal_mantenimiento")
+@NoArgsConstructor
+@AllArgsConstructor
+public class MaintenanceStaffEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nombre", nullable = false)
+    private String firstName;
+
+    @Column(name = "apellido", nullable = false)
+    private String lastName;
+
+    private String email;
+
+    private String occupation;
+
+    @Column(nullable = false)
+    private boolean availability; // "available" or "busy"
+
+    @Column(name = "mantenimientos_completados")
+    private Integer maintenanceCompleted;
+
+    private String signaturePath;
+
+    @OneToMany(mappedBy = "maintenanceStaff", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Order> orderList;
+
+}
