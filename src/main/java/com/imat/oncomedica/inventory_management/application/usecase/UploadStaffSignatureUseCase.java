aed6dbcd7edcf996b836b67468ff7e847fff5841
rafael07-dev <deralato817@gmail.com>
2025-07-11 11:13:14 -0500

    usando repositorio staff

--- src/main/java/com/imat/oncomedica/inventory_management/application/usecase/UploadStaffSignatureUseCase.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/usecase/UploadStaffSignatureUseCase.java
@@ -1,8 +1,8 @@
 package com.imat.oncomedica.inventory_management.application.usecase;
 
 import com.imat.oncomedica.inventory_management.domain.exception.MaintenanceStaffNotFound;
+import com.imat.oncomedica.inventory_management.domain.repository.MaintenanceStaffRepository;
 import com.imat.oncomedica.inventory_management.domain.service.FileStorageService;
-import com.imat.oncomedica.inventory_management.infrastructure.repository.MaintenanceStaffRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,22 +10,23 @@ import org.springframework.web.multipart.MultipartFile;
 public class UploadStaffSignatureUseCase {
 
     private final FileStorageService fileStorageService;
-    private final MaintenanceStaffRepository managementStaffRepository;
+    private final MaintenanceStaffRepository maintenanceStaffRepository;
 
-    public UploadStaffSignatureUseCase(FileStorageService fileStorageService, MaintenanceStaffRepository managementStaffRepository) {
+    public UploadStaffSignatureUseCase(FileStorageService fileStorageService, MaintenanceStaffRepository maintenanceStaffRepository) {
         this.fileStorageService = fileStorageService;
-        this.managementStaffRepository = managementStaffRepository;
+        this.maintenanceStaffRepository = maintenanceStaffRepository;
     }
 
+
     public String execute(MultipartFile file, Integer id){
-        var staff = managementStaffRepository.findById(id)
+        var staff = maintenanceStaffRepository.findById(id)
                 .orElseThrow(() -> new MaintenanceStaffNotFound(id));
 
         var pathUrl = fileStorageService.uploadStaffSignature(id, file);
 
         staff.setSignaturePath(pathUrl);
 
-        managementStaffRepository.save(staff);
+        maintenanceStaffRepository.save(staff);
         return pathUrl;
     }
 }
