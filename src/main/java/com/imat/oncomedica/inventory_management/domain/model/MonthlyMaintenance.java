a0fa48408093d4de3547ef6607c6337bd35b34ac
rafael07-dev <deralato817@gmail.com>
2025-07-11 17:40:09 -0500

    migrar entidades a insfraestructura para evitar usar spring en domain

--- src/main/java/com/imat/oncomedica/inventory_management/domain/model/MonthlyMaintenance.java
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/model/MonthlyMaintenance.java
@@ -1,25 +1,19 @@
 package com.imat.oncomedica.inventory_management.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import java.io.Serializable;
 import java.util.List;
 
-@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MonthlyMaintenance implements Serializable {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @ManyToOne
-    @JoinColumn(name = "schedule_id")
     @JsonIgnore
     private MaintenanceSchedule maintenanceSchedule;
 
@@ -27,7 +21,6 @@ public class MonthlyMaintenance implements Serializable {
 
     private Integer year;
 
-    @OneToMany(mappedBy = "monthlyMaintenance", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<MonthlyMaintenanceType> maintenanceTypes;
 
 }
--- src/main/java/com/imat/oncomedica/inventory_management/domain/model/MonthlyMaintenanceType.java
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/model/MonthlyMaintenanceType.java
@@ -6,21 +6,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MonthlyMaintenanceType {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Enumerated(EnumType.STRING)
     private MaintenanceTypeEnum maintenanceTypeEnum;
 
-    @ManyToOne
-    @JoinColumn(name = "monthly_maintenance_id")
     @JsonIgnore
     private MonthlyMaintenance monthlyMaintenance;
 
