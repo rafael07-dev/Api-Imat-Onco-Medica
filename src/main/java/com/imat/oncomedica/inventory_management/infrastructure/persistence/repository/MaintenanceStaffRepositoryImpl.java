3f862dad7a4958b51df75c4f4f509f6400539ca7
rafael07-dev <deralato817@gmail.com>
2025-07-11 11:06:24 -0500

    implementando metodos de interfas repositorio de staff

---
+++ src/main/java/com/imat/oncomedica/inventory_management/infrastructure/persistence/repository/MaintenanceStaffRepositoryImpl.java
@@ -0,0 +1,66 @@
+package com.imat.oncomedica.inventory_management.infrastructure.persistence.repository;
+
+import com.imat.oncomedica.inventory_management.application.dto.staff.CreateMaintenanceStaffRequest;
+import com.imat.oncomedica.inventory_management.application.dto.staff.UpdateMaintenanceStaffRequest;
+import com.imat.oncomedica.inventory_management.domain.exception.MaintenanceStaffNotFound;
+import com.imat.oncomedica.inventory_management.domain.model.MaintenanceStaff;
+import com.imat.oncomedica.inventory_management.domain.repository.MaintenanceStaffRepository;
+import com.imat.oncomedica.inventory_management.infrastructure.persistence.entity.MaintenanceStaffEntity;
+import com.imat.oncomedica.inventory_management.infrastructure.persistence.mapper.MaintenanceStaffPersistenceMapper;
+import java.util.List;
+import java.util.Optional;
+
+public class MaintenanceStaffRepositoryImpl implements MaintenanceStaffRepository {
+
+    private final SpringDataMaintenanceStaffRepository maintenanceStaffRepository;
+    private final MaintenanceStaffPersistenceMapper maintenanceStaffMapper;
+
+    public MaintenanceStaffRepositoryImpl(SpringDataMaintenanceStaffRepository maintenanceStaffRepository, MaintenanceStaffPersistenceMapper maintenanceStaffMapper) {
+        this.maintenanceStaffRepository = maintenanceStaffRepository;
+        this.maintenanceStaffMapper = maintenanceStaffMapper;
+    }
+
+    @Override
+    public MaintenanceStaff findByName(String name) {
+        return maintenanceStaffRepository.findByFirstName(name)
+                .map(maintenanceStaffMapper::toModel)
+                .orElseThrow(() -> new MaintenanceStaffNotFound("Maintenance staff not found"));
+    }
+
+    @Override
+    public List<MaintenanceStaff> findAll() {
+        return maintenanceStaffRepository.findAll()
+                .stream()
+                .map(maintenanceStaffMapper::toModel)
+                .toList();
+    }
+
+    @Override
+    public MaintenanceStaff save(CreateMaintenanceStaffRequest request) {
+        MaintenanceStaffEntity ms = maintenanceStaffMapper.toEntity(request);
+
+        if (ms == null){
+            throw new MaintenanceStaffNotFound();
+        }
+
+        maintenanceStaffRepository.save(ms);
+
+        return maintenanceStaffMapper.toModel(ms);
+    }
+
+    @Override
+    public MaintenanceStaff update(UpdateMaintenanceStaffRequest request, Integer id) {
+        var maintenanceStaffSaved = maintenanceStaffRepository.findById(id)
+                .orElseThrow(() -> new MaintenanceStaffNotFound(id));
+
+        var maintenanceStaffUpdated = maintenanceStaffMapper.updateMaintenanceStaffFromRequest(request, maintenanceStaffSaved);
+        var staffSaved = maintenanceStaffRepository.save(maintenanceStaffUpdated);
+        return maintenanceStaffMapper.toModel(staffSaved);
+    }
+
+    @Override
+    public Optional<MaintenanceStaff> findById(Integer id) {
+        return maintenanceStaffRepository.findById(id)
+                .map(maintenanceStaffMapper::toModel);
+    }
+}
