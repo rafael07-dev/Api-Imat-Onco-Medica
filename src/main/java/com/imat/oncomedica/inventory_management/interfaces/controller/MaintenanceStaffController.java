cc8ea988d2ac75ae02e7a23337a4422cee13c561
rafael07-dev <deralato817@gmail.com>
2025-07-11 17:28:55 -0500

    usando casos de uso de staff

--- src/main/java/com/imat/oncomedica/inventory_management/interfaces/controller/MaintenanceStaffController.java
+++ src/main/java/com/imat/oncomedica/inventory_management/interfaces/controller/MaintenanceStaffController.java
@@ -3,8 +3,7 @@ package com.imat.oncomedica.inventory_management.interfaces.controller;
 import com.imat.oncomedica.inventory_management.application.dto.staff.CreateMaintenanceStaffRequest;
 import com.imat.oncomedica.inventory_management.application.dto.staff.MaintenanceStaffResponse;
 import com.imat.oncomedica.inventory_management.application.dto.staff.UpdateMaintenanceStaffRequest;
-import com.imat.oncomedica.inventory_management.application.usecase.UploadStaffSignatureUseCase;
-import com.imat.oncomedica.inventory_management.domain.service.MaintenanceStaffService;
+import com.imat.oncomedica.inventory_management.application.usecase.staff.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,34 +14,44 @@ import java.util.List;
 @RequestMapping("api/maintenance-staff")
 public class MaintenanceStaffController {
 
-    private final MaintenanceStaffService maintenanceStaffService;
+    private final CreateMaintenanceStaffUseCase createMaintenanceStaffUseCase;
+    private final GetAllMaintenanceStaffUseCase getAllMaintenanceStaffUseCase;
+    private final GetMaintenanceStaffByIdUseCase getMaintenanceStaffByIdUseCase;
+    private final UpdateMaintenanceStaffUseCase updateMaintenanceStaffUseCase;
     private final UploadStaffSignatureUseCase uploadStaffSignatureUseCase;
 
-    public MaintenanceStaffController(MaintenanceStaffService maintenanceStaffService, UploadStaffSignatureUseCase uploadStaffSignatureUseCase) {
-        this.maintenanceStaffService = maintenanceStaffService;
+    public MaintenanceStaffController(CreateMaintenanceStaffUseCase createMaintenanceStaffUseCase,
+                                      GetAllMaintenanceStaffUseCase getAllMaintenanceStaffUseCase,
+                                      GetMaintenanceStaffByIdUseCase getMaintenanceStaffByIdUseCase,
+                                      UpdateMaintenanceStaffUseCase updateMaintenanceStaffUseCase,
+                                      UploadStaffSignatureUseCase uploadStaffSignatureUseCase) {
+        this.createMaintenanceStaffUseCase = createMaintenanceStaffUseCase;
+        this.getAllMaintenanceStaffUseCase = getAllMaintenanceStaffUseCase;
+        this.getMaintenanceStaffByIdUseCase = getMaintenanceStaffByIdUseCase;
+        this.updateMaintenanceStaffUseCase = updateMaintenanceStaffUseCase;
         this.uploadStaffSignatureUseCase = uploadStaffSignatureUseCase;
     }
 
     @PostMapping("/create")
     public ResponseEntity<MaintenanceStaffResponse> save(@RequestBody CreateMaintenanceStaffRequest request){
-        MaintenanceStaffResponse maintenanceStaffSaved = maintenanceStaffService.createMaintenanceStaff(request);
+        MaintenanceStaffResponse maintenanceStaffSaved = createMaintenanceStaffUseCase.execute(request);
         return ResponseEntity.created(URI.create("/api/maintenance-staff/" + maintenanceStaffSaved.getId()))
                 .body(maintenanceStaffSaved);
     }
 
     @PutMapping(value = "/update/{id}", produces = "application/json")
     public ResponseEntity<MaintenanceStaffResponse> updateStaff(@RequestBody UpdateMaintenanceStaffRequest request, @PathVariable Integer id){
-        return ResponseEntity.ok().body(maintenanceStaffService.updateMaintenanceStaff(request, id));
+        return ResponseEntity.ok().body(updateMaintenanceStaffUseCase.execute(request, id));
     }
 
     @GetMapping("/")
     public ResponseEntity<List<MaintenanceStaffResponse>> findAll(){
-        return ResponseEntity.ok().body(maintenanceStaffService.getAllMaintenanceStaff());
+        return ResponseEntity.ok().body(getAllMaintenanceStaffUseCase.execute());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<MaintenanceStaffResponse> getById(@PathVariable Integer id){
-        return ResponseEntity.ok().body(maintenanceStaffService.getMaintenanceStaffById(id));
+        return ResponseEntity.ok().body(getMaintenanceStaffByIdUseCase.execute(id));
     }
 
     @PostMapping(value = "/upload-signature/{id}", consumes = "multipart/form-data")
