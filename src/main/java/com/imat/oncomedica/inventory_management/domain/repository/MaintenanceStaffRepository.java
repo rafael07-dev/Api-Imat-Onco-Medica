9f4e05a537f90538b7826036240fdb2b95d28654
rafael07-dev <deralato817@gmail.com>
2025-07-11 17:29:41 -0500

    usando modelo de domain

--- src/main/java/com/imat/oncomedica/inventory_management/domain/repository/MaintenanceStaffRepository.java
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/repository/MaintenanceStaffRepository.java
@@ -1,6 +1,5 @@
 package com.imat.oncomedica.inventory_management.domain.repository;
 
-import com.imat.oncomedica.inventory_management.application.dto.staff.CreateMaintenanceStaffRequest;
 import com.imat.oncomedica.inventory_management.application.dto.staff.UpdateMaintenanceStaffRequest;
 import com.imat.oncomedica.inventory_management.domain.model.MaintenanceStaff;
 import java.util.List;
@@ -10,7 +9,7 @@ public interface MaintenanceStaffRepository {
 
     MaintenanceStaff findByName(String name);
     List<MaintenanceStaff> findAll();
-    MaintenanceStaff save(CreateMaintenanceStaffRequest maintenanceStaff);
+    MaintenanceStaff save(MaintenanceStaff maintenanceStaff);
     MaintenanceStaff update(UpdateMaintenanceStaffRequest maintenanceStaff, Integer id);
     Optional<MaintenanceStaff> findById(Integer id);
 }
