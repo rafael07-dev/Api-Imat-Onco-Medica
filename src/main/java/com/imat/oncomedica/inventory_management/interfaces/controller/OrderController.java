babacd1a700214d8e779a78c8f990e16e052112e
rafael07-dev <deralato817@gmail.com>
2025-07-21 19:39:38 -0500

    usando casos de uso de Order

--- src/main/java/com/imat/oncomedica/inventory_management/interfaces/controller/OrderController.java
+++ src/main/java/com/imat/oncomedica/inventory_management/interfaces/controller/OrderController.java
@@ -1,7 +1,9 @@
 package com.imat.oncomedica.inventory_management.interfaces.controller;
 
 import com.imat.oncomedica.inventory_management.application.dto.order.OrderResponse;
-import com.imat.oncomedica.inventory_management.domain.service.OrderService;
+import com.imat.oncomedica.inventory_management.application.usecase.order.GetAllOrdersUseCase;
+import com.imat.oncomedica.inventory_management.application.usecase.order.GetOrderByIdUseCase;
+import com.imat.oncomedica.inventory_management.application.usecase.order.GetOrderByStaffIdUseCase;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,27 +15,32 @@ import java.util.List;
 @RequestMapping("/api/orders/")
 public class OrderController {
 
-    private final OrderService orderService;
+    private final GetAllOrdersUseCase getAllOrdersUseCase;
+    private final GetOrderByIdUseCase getOrderByIdUseCase;
+    private final GetOrderByStaffIdUseCase getOrderByStaffIdUseCase;
 
-    public OrderController(OrderService orderService) {
-        this.orderService = orderService;
+    public OrderController(GetAllOrdersUseCase getAllOrdersUseCase, GetOrderByIdUseCase getOrderByIdUseCase, GetOrderByStaffIdUseCase getOrderByStaffIdUseCase) {
+        this.getAllOrdersUseCase = getAllOrdersUseCase;
+        this.getOrderByIdUseCase = getOrderByIdUseCase;
+        this.getOrderByStaffIdUseCase = getOrderByStaffIdUseCase;
     }
+
 
     @GetMapping
     public ResponseEntity<List<OrderResponse>> getAllOrders(){
         return ResponseEntity.ok()
-                .body(orderService.getAllOrders());
+                .body(getAllOrdersUseCase.execute());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<OrderResponse> getOrderById(@PathVariable Integer id){
         return ResponseEntity.ok()
-                .body(orderService.getOrderById(id));
+                .body(getOrderByIdUseCase.execute(id));
     }
 
     @GetMapping("/staff/{id}")
     public ResponseEntity<List<OrderResponse>> getOrderByStaffId(@PathVariable Integer id){
         return ResponseEntity.ok()
-                .body(orderService.getOrdersByStaffId(id));
+                .body(getOrderByStaffIdUseCase.execute(id));
     }
 }
