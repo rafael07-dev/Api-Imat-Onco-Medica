66bdcd60c314f75694134e6b32052e76f9029fff
rafael07-dev <deralato817@gmail.com>
2025-07-21 19:21:58 -0500

    obtener order por id de mantenimiento

--- src/main/java/com/imat/oncomedica/inventory_management/application/usecase/maintenance/CompleteMaintenanceUseCase.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/usecase/maintenance/CompleteMaintenanceUseCase.java
@@ -3,11 +3,12 @@ package com.imat.oncomedica.inventory_management.application.usecase.maintenance
 import com.imat.oncomedica.inventory_management.application.dto.maintenance.MaintenanceResponse;
 import com.imat.oncomedica.inventory_management.application.dto.maintenance.UpdateMaintenanceRequest;
 import com.imat.oncomedica.inventory_management.application.mapper.MaintenanceMapper;
+import com.imat.oncomedica.inventory_management.domain.exception.OrderNotFoundException;
 import com.imat.oncomedica.inventory_management.domain.model.OrderStatus;
 import com.imat.oncomedica.inventory_management.domain.exception.MaintenanceStaffNotFound;
 import com.imat.oncomedica.inventory_management.domain.repository.MaintenanceRepository;
+import com.imat.oncomedica.inventory_management.domain.repository.OrderRepository;
 import com.imat.oncomedica.inventory_management.infrastructure.report.OrderPdfGenerator;
-import com.imat.oncomedica.inventory_management.infrastructure.repository.OrderRepository;
 
 public class CompleteMaintenanceUseCase {
 
@@ -22,6 +23,7 @@ public class CompleteMaintenanceUseCase {
         this.orderPdfGenerator = orderPdfGenerator;
         this.orderRepository = orderRepository;
     }
+
 
     public MaintenanceResponse execute(UpdateMaintenanceRequest request, Integer maintenanceId) {
 
@@ -38,11 +40,12 @@ public class CompleteMaintenanceUseCase {
         var maintenanceSaved = maintenanceRepository.save(maintenanceEntity);
 
 
-        var order = orderRepository.findByMaintenance_Id(maintenanceId);
+        var order = orderRepository.getOrderByMaintenanceId(maintenanceId)
+                .orElseThrow(() -> new OrderNotFoundException(maintenanceId));
 
         order.setStatus(OrderStatus.COMPLETED);
 
-        var orderSaved = orderRepository.save(order);
+        var orderSaved = orderRepository.saveOrder(order);
 
         orderPdfGenerator.generateOderReport(orderSaved);
 
