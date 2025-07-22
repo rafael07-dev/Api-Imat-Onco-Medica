805e61432b6a8bfa4b3248edc708579cd8b98d88
rafael07-dev <deralato817@gmail.com>
2025-07-21 19:37:08 -0500

    agregar anotacion @repository

--- src/main/java/com/imat/oncomedica/inventory_management/infrastructure/persistence/repository/MaintenanceStaffRepositoryImpl.java
+++ src/main/java/com/imat/oncomedica/inventory_management/infrastructure/persistence/repository/MaintenanceStaffRepositoryImpl.java
@@ -6,9 +6,12 @@ import com.imat.oncomedica.inventory_management.domain.model.MaintenanceStaff;
 import com.imat.oncomedica.inventory_management.domain.repository.MaintenanceStaffRepository;
 import com.imat.oncomedica.inventory_management.infrastructure.persistence.entity.MaintenanceStaffEntity;
 import com.imat.oncomedica.inventory_management.infrastructure.persistence.mapper.MaintenanceStaffPersistenceMapper;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class MaintenanceStaffRepositoryImpl implements MaintenanceStaffRepository {
 
     private final SpringDataMaintenanceStaffRepository maintenanceStaffRepository;
