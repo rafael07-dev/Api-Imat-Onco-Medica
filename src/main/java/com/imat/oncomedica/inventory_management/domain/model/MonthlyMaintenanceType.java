6e724ca67eb426c1e19c215636ebc4c471e5d12a
rafael07-dev <deralato817@gmail.com>
2025-07-21 19:27:40 -0500

    convertir entidad a POJO

--- src/main/java/com/imat/oncomedica/inventory_management/domain/model/MonthlyMaintenanceType.java
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/model/MonthlyMaintenanceType.java
@@ -1,7 +1,5 @@
 package com.imat.oncomedica.inventory_management.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,12 +10,7 @@ import lombok.NoArgsConstructor;
 public class MonthlyMaintenanceType {
 
     private int id;
-
-    @Enumerated(EnumType.STRING)
     private MaintenanceTypeEnum maintenanceTypeEnum;
-
-    @JsonIgnore
     private MonthlyMaintenance monthlyMaintenance;
-
     private Integer quantity;
 }
