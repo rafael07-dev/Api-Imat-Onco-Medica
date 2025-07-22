da5c5797c3484efa610ff5820cf6c45f5779f54d
rafael07-dev <deralato817@gmail.com>
2025-07-21 19:24:42 -0500

    cambiar nombre de metodo de caso de uso a "execute"

--- src/main/java/com/imat/oncomedica/inventory_management/application/usecase/maintenance/UploadMaintenanceImageUseCase.java
+++ src/main/java/com/imat/oncomedica/inventory_management/application/usecase/maintenance/UploadMaintenanceImageUseCase.java
@@ -3,7 +3,6 @@ package com.imat.oncomedica.inventory_management.application.usecase.maintenance
 import com.imat.oncomedica.inventory_management.domain.exception.MaintenanceNotFoundException;
 import com.imat.oncomedica.inventory_management.domain.repository.MaintenanceRepository;
 import com.imat.oncomedica.inventory_management.domain.service.FileStorageService;
-import com.imat.oncomedica.inventory_management.infrastructure.persistence.repository.SpringDataMaintenanceRepository;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,8 +17,7 @@ public class UploadMaintenanceImageUseCase {
         this.maintenanceRepository = maintenanceRepository;
     }
 
-
-    public String UploadMaintenanceImageUseCase(MultipartFile file, Integer maintenanceId) {
+    public String execute(MultipartFile file, Integer maintenanceId) {
         var url = fileStorageService.uploadMaintenanceImage(maintenanceId, file);
 
         var maintenanceSaved = maintenanceRepository.findById(maintenanceId)
