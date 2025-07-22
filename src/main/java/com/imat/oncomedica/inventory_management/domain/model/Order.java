b53eabe9e6f9b2f31432aa083708ee7ff5c30e5f
rafael07-dev <deralato817@gmail.com>
2025-07-21 19:28:09 -0500

    convertir a clase POJO

--- src/main/java/com/imat/oncomedica/inventory_management/domain/model/Order.java
+++ src/main/java/com/imat/oncomedica/inventory_management/domain/model/Order.java
@@ -1,32 +1,15 @@
 package com.imat.oncomedica.inventory_management.domain.model;
 
-import jakarta.persistence.*;
 import lombok.Data;
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "ordenes")
 @Data
 public class Order {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     private LocalDateTime creationDate;
-
-    @ManyToOne
-    @JoinColumn(name = "staff_id")
     private MaintenanceStaff maintenanceStaff;
-
-    @ManyToOne
-    @JoinColumn(name = "equipment_id")
     private Equipment equipment;
-
-    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
-    @JoinColumn(name = "maintenance_id")
     private Maintenance maintenance;
-
-    @Enumerated(EnumType.STRING)
     private OrderStatus status;
 }
